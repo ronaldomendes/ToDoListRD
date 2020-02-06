4ecad353fdfe02c9a6a46ac6a19e50bc36446759
Treinamento <Treinamento@RD723903.raiadrogasil.com>
2020-02-06 16:57:51 -0300

    Finalizando o projeto

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,81 @@
 const btnAdd = document.querySelector('#adicionar-tarefa')
-const btnRemove = document.querySelectorAll('.botao-tarefa')
 const novaTarefa = document.querySelector('#tarefas')
 const msgTarefa = document.querySelector('#tarefa-digitada')
 
-btnAdd.onclick = () => {
-    const divCard = `
-        <div class="col-md-4">
-            <div class="card-tarefa">
-                <div class="texto-tarefa text-wrap">
-                    ${msgTarefa.value}
-                </div>
-                <div class="botao-tarefa">
-                    <img src="./img/checkmark.png" alt="Finalizar tarefa" title="Finalizar tarefa" width="32">
-                </div>
-            </div>
-        </div>`
-        novaTarefa.innerHTML += divCard
-        msgTarefa.value = ''
+// lista para salvar cada tarefa
+let listaTarefas = localStorage.getItem('tarefa') ? JSON.parse(localStorage.getItem('tarefa')) : []
+
+// adicionando o valor de cada tarefa no Local Storage
+const salvarLocalStorage = (arrayTarefas) => {
+    let tarefasJSON = JSON.stringify(arrayTarefas)
+    localStorage.setItem('tarefa', tarefasJSON)
+    // console.log('Lista de tarefas salva com sucesso!')
 }
 
-btnRemove[0].onclick = () => {
-    console.log('clicou para remover')
+// mostrar todas as tarefas salvas na tela
+const mostrarTarefasNaTela = (arrayTarefas) => {
+    arrayTarefas.forEach((tarefa) => {
+        const divCard =
+            `<div class="col-md-4">
+                <div class="card-tarefa">
+                    <div class="texto-tarefa text-wrap overflow-auto">
+                        ${tarefa}
+                    </div>
+                    <div class="botao-tarefa">
+                        <img src="./img/checkmark.png" alt="Finalizar tarefa" title="Finalizar tarefa" width="32">
+                    </div>
+                </div>
+            </div>`
+        novaTarefa.insertAdjacentHTML('beforeend', divCard)
+        const btnCheck = novaTarefa.lastElementChild.lastElementChild.lastElementChild
+        btnCheck.onclick = (event) => {
+            const boxTarefa = event.target.parentNode.parentNode.parentNode
+            novaTarefa.removeChild(boxTarefa)
+
+            listaTarefas = listaTarefas.filter((valor) => valor != tarefa)
+            salvarLocalStorage(listaTarefas)
+        }
+    })
 }
 
+mostrarTarefasNaTela(listaTarefas)
+
+const tarefaEnterClick = (event) => {
+    if (event.keyCode === 13 || event.type === 'click') {
+        if (msgTarefa.value != '') {
+            listaTarefas.push(msgTarefa.value) // adicionando uma tarefa
+            salvarLocalStorage(listaTarefas)
+            const divCard =
+                `<div class="col-md-4">
+                    <div class="card-tarefa">
+                        <div class="texto-tarefa text-wrap overflow-auto">
+                            ${msgTarefa.value}
+                        </div>
+                        <div class="botao-tarefa">
+                            <img src="./img/checkmark.png" alt="Finalizar tarefa" title="Finalizar tarefa" width="32">
+                        </div>
+                    </div>
+                </div>`
+            novaTarefa.insertAdjacentHTML('beforeend', divCard)
+
+            // referência para acessar a classe "botao-tarefa"
+            const btnCheck = novaTarefa.lastElementChild.lastElementChild.lastElementChild
+            btnCheck.onclick = (event) => {
+                // referência para acessar a classe "col-md-4"
+                const boxTarefa = event.target.parentNode.parentNode.parentNode
+                // removendo a div com a classe "col-md-4"
+                novaTarefa.removeChild(boxTarefa)
+                // comparando os valores da lista para exclusão
+                listaTarefas = listaTarefas.filter((valor) => valor != msgTarefa.value)
+                // salvando uma nova lista 
+                salvarLocalStorage(listaTarefas)
+            }
+            msgTarefa.value = ''
+        } else {
+            alert('Preencha uma tarefa antes de clicar!')
+        }
+    }
+}
+
+msgTarefa.addEventListener('keypress', tarefaEnterClick)
+btnAdd.addEventListener('click', tarefaEnterClick)
